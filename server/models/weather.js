4c7a48c5a602576e28143afa157e7236822219dd
Sheikh, Haris <haris.sheikh@accenture.com>
2019-05-16 18:15:03 +0100

    Fully functioning back end code.

--- server/models/weather.js
+++ server/models/weather.js
@@ -1,9 +1,8 @@
 const request = require('request-promise');
 
-const API_KEY = "50a5d0165878cd27f5f7bb94df7600d6"
-require('dotenv').config();
+const API_KEY = '50a5d0165878cd27f5f7bb94df7600d6';
 
-class Weather { 
+class Weather {
   static retrieveByCity (city, callback) {
     request({
       uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`,

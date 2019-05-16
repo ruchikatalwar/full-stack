ddc9b70508752bfffa659638a1014cf66103dc9f
Sheikh, Haris <haris.sheikh@accenture.com>
2019-05-16 16:50:57 +0100

    commit 4

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+var db = require('./database');
+
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
 
@@ -10,8 +12,16 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use('/api/cities', require('./api/cities'));
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
+});
+
+db.query('SELECT NOW()', (err, res) => {
+  if (err.error)
+    return console.log(err.error);
+  console.log(`PostgreSQL connected: ${res[0].now}.`);
 });
 
 module.exports = app;

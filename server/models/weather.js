620a48b539f7ac6e7b5e1dc82a3de2da0184d974
Sheikh, Haris <haris.sheikh@accenture.com>
2019-05-16 17:58:37 +0100

    commit 5.

---
+++ server/models/weather.js
@@ -0,0 +1,20 @@
+const request = require('request-promise');
+
+const API_KEY = "50a5d0165878cd27f5f7bb94df7600d6"
+require('dotenv').config();
+
+class Weather { 
+  static retrieveByCity (city, callback) {
+    request({
+      uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`,
+      json: true
+    }).then((res) => {
+      callback(res);
+    }).catch((err) => {
+      console.log(err);
+      callback({ error: 'Could not reach OpenWeatherMap API.' });
+    });
+  }
+}
+
+module.exports = Weather;

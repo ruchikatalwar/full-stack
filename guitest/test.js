19842167376fb5fd75c8c0843832ec7a27824486
Sheikh, Haris <haris.sheikh@accenture.com>
2019-05-26 23:54:36 +0100

    process.exit

--- guitest/test.js
+++ guitest/test.js
@@ -2,12 +2,15 @@ var webdriverio = require('webdriverio')
 var options = { desiredCapabilities:  {browserName: 'chrome' }};
 var client = webdriverio.remote(options);
 
+
 client
 .init()
-.url('')
-.setValue('#id-1-1', 'Barcelona')
-.click("id-1-2")
-.click('#id-1-3')
+.url('https://weather-app-hs2019.herokuapp.com/')
+.setValue('#id-1-1', 'Spain')
+.click("#id-1-2")
+
+
+
 
 // need to add selection of barcelona from drop down list
 
